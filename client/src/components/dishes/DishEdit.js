4e306acffc5edb40b40814bb9c038ece4b5c0d3b
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2021-06-10 21:08:19 +0100

    swipable views

--- client/src/components/dishes/DishEdit.js
+++ client/src/components/dishes/DishEdit.js
@@ -1,32 +1,58 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { changeHeaderTitle } from "../../actions";
 
 // Material UI Core
-import { makeStyles } from "@material-ui/core/styles";
+import { useTheme } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
-import Typography from "@material-ui/core/Typography";
+import SwipeableViews from "react-swipeable-views";
 import Box from "@material-ui/core/Box";
-
-const useStyles = makeStyles((theme) => ({
-  root: {},
-}));
-
+import Typography from "@material-ui/core/Typography";
 const DishEdit = ({ changeHeaderTitle }) => {
+  const theme = useTheme();
   //State
+  const [value, setValue] = useState(0);
+
   useEffect(() => {
     changeHeaderTitle("Prep list");
   }, [changeHeaderTitle]);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index) => {
+    setValue(index);
+  };
+
+  function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box p={3}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
 
   return (
     <div>
       <AppBar position="static">
         <Tabs
-          centered
+          onChange={handleChange}
           variant="fullWidth"
-          value={2}
+          value={value}
           aria-label="simple tabs example"
         >
           <Tab label="Teppan" />
@@ -34,6 +60,21 @@ const DishEdit = ({ changeHeaderTitle }) => {
           <Tab label="Fry" />
         </Tabs>
       </AppBar>
+      <SwipeableViews
+        axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+        index={value}
+        onChangeIndex={handleChangeIndex}
+      >
+        <TabPanel value={value} index={0} dir={theme.direction}>
+          Teppan
+        </TabPanel>
+        <TabPanel value={value} index={1} dir={theme.direction}>
+          Wok
+        </TabPanel>
+        <TabPanel value={value} index={2} dir={theme.direction}>
+          Fry
+        </TabPanel>
+      </SwipeableViews>
     </div>
   );
 };
