cd0638b074b28d15e3cf0240ab823ebacbf05868
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2021-06-10 16:55:50 +0100

    Dish list component finished

--- client/src/components/dishes/DishList.js
+++ client/src/components/dishes/DishList.js
@@ -14,51 +14,66 @@ import TableRow from "@material-ui/core/TableRow";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginBottom: "1.5rem",
+  },
+}));
+
 const DishList = ({ fetchDishes, changeHeaderTitle, dishes }) => {
   // State
   useEffect(() => {
     fetchDishes();
     changeHeaderTitle("Home");
   }, [fetchDishes, changeHeaderTitle]);
-  console.log(dishes);
 
-  const sections = ["teppan", "wok", "fry"];
+  // Styles
+  const classes = useStyles();
+
+  const sections = ["Teppan", "Wok", "Fry"];
 
   // Return statement
   return (
-    <>
-      <TableContainer component={Paper} elevation={5}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Section</TableCell>
-              <TableCell align="right">Have</TableCell>
-              <TableCell align="right">Need</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {dishes.map((dish) => {
-              if (dish.section === "teppan") {
-                return (
-                  <TableRow>
-                    <TableCell>
-                      <Typography>{dish.name}</Typography>
-                    </TableCell>
-                    <TableCell align="right">
-                      <Typography>{dish.currentAmount}</Typography>
-                    </TableCell>
-                    <TableCell align="right">
-                      <Typography>{dish.neededAmount}</Typography>
-                    </TableCell>
-                  </TableRow>
-                );
-              }
-              return null;
-            })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </>
+    <div>
+      {sections.map((section) => {
+        return (
+          <TableContainer
+            className={classes.root}
+            key={section}
+            component={Paper}
+            elevation={5}
+          >
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>{section}</TableCell>
+                  <TableCell align="right">Have</TableCell>
+                  <TableCell align="right">Need</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {dishes.map((dish) => {
+                  if (dish.section === section.toLowerCase()) {
+                    return (
+                      <TableRow key={dish._id}>
+                        <TableCell>{dish.name}</TableCell>
+                        <TableCell align="right">
+                          <Typography>{dish.currentAmount}</Typography>
+                        </TableCell>
+                        <TableCell align="right">
+                          <Typography>{dish.neededAmount}</Typography>
+                        </TableCell>
+                      </TableRow>
+                    );
+                  }
+                  return null;
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        );
+      })}
+    </div>
   );
 };
 
--- client/src/components/dishes/PrepListOpening.js
+++ client/src/components/dishes/PrepListOpening.js
@@ -2,6 +2,18 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { changeHeaderTitle } from "../../actions";
 
+// Material UI Core
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+// --Utils
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+
 const PrepListOpening = ({ changeHeaderTitle }) => {
   useEffect(() => {
     changeHeaderTitle("Morning prep list");
