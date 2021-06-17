5c13a88d79d0c997ba8b3ba97cad313e70d5cd2a
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2021-06-17 20:47:52 +0100

    Card filtering

--- client/src/components/dishes/DishList.js
+++ client/src/components/dishes/DishList.js
@@ -19,6 +19,7 @@ import {
   FormControlLabel,
   Radio,
   Divider,
+  Button,
 } from "@material-ui/core";
 
 // Material UI Icons
@@ -27,96 +28,140 @@ import EditIcon from "@material-ui/icons/Edit";
 
 import history from "../../history";
 
-const menuOptions = [
-  {
-    text: "Add item",
-    action: () => history.push("/add"),
-  },
-  {
-    text: "Search...",
-    action: () => history.push("/search"),
-  },
-];
-
 const useStyle = makeStyles((theme) => ({
   "p:firstLetter": {
     textTransform: "capitalize",
   },
 }));
 
-const DishList = ({
-  fetchDishes,
-  changeHeaderTitle,
-  getMenuOptions,
-  dishes,
-}) => {
+const DishList = (props) => {
+  const { fetchDishes, changeHeaderTitle, getMenuOptions, dishes } = props;
   // State
+  const [radioValue, setRadioValue] = useState("all");
+  const [openFilterOptions, setOpenFilterOptions] = useState(false);
+  const [filteredOption, setFilteredOption] = useState(radioValue);
+  const classes = useStyle();
+
   useEffect(() => {
+    const menuOptions = [
+      {
+        text: "Add item",
+        action: () => history.push("/add"),
+      },
+      {
+        text: "Filter",
+        action: () => setOpenFilterOptions(true),
+      },
+    ];
     fetchDishes();
     changeHeaderTitle("Home");
     getMenuOptions(menuOptions);
   }, [fetchDishes, changeHeaderTitle, getMenuOptions]);
 
-  const [sections, setSections] = useState(["Teppan", "Wok", "Fry"]);
-  const classes = useStyle();
+  const changeRadioValue = (e) => setRadioValue(e.target.value);
+
+  const onClickFilter = () => {
+    setOpenFilterOptions(false);
+    setFilteredOption(radioValue);
+  };
 
   // Return statement
   return (
     <Container>
-      <FormControl component="fieldset">
-        <FormLabel component="legend">Filter by section</FormLabel>
-        <RadioGroup
-          row
-          aria-label="position"
-          name="position"
-          defaultValue="top"
-        >
-          <FormControlLabel
-            value="all"
-            control={<Radio color="secondary" />}
-            label="All"
-          />
-          <FormControlLabel
-            value="teppan"
-            control={<Radio color="secondary" />}
-            label="Teppan"
-          />
-          <FormControlLabel
-            value="wok"
-            control={<Radio color="secondary" />}
-            label="Wok"
-          />
-          <FormControlLabel
-            value="fry"
-            control={<Radio color="secondary" />}
-            label="Fry"
-          />
-        </RadioGroup>
-      </FormControl>
-      <Divider style={{ marginBottom: "10px" }} />
+      {openFilterOptions && (
+        <div>
+          <FormControl component="fieldset">
+            <FormLabel component="legend">Filter by section</FormLabel>
+            <RadioGroup
+              row
+              aria-label="position"
+              name="position"
+              value={radioValue}
+            >
+              <FormControlLabel
+                value="all"
+                control={<Radio color="secondary" />}
+                label="All"
+                onChange={(e) => changeRadioValue(e)}
+              />
+              <FormControlLabel
+                value="teppan"
+                control={<Radio color="secondary" />}
+                label="Teppan"
+                onChange={(e) => changeRadioValue(e)}
+              />
+              <FormControlLabel
+                value="wok"
+                control={<Radio color="secondary" />}
+                label="Wok"
+                onChange={(e) => changeRadioValue(e)}
+              />
+              <FormControlLabel
+                value="fry"
+                control={<Radio color="secondary" />}
+                label="Fry"
+                onChange={(e) => changeRadioValue(e)}
+              />
+            </RadioGroup>
+          </FormControl>
+          <br />
+          <Button variant="contained" color="primary" onClick={onClickFilter}>
+            Save
+          </Button>
+        </div>
+      )}
+      <Divider style={{ marginBottom: "10px", marginTop: "10px" }} />
       <Grid container spacing={5}>
-        {dishes.map((dish) => (
-          <Grid key={dish.name} item xs={12} md={6} lg={4}>
-            <Card elevation={5}>
-              <CardHeader
-                className={classes["p:firstLetter"]}
-                title={dish.name}
-                subheader={dish.section}
-              ></CardHeader>
-              <CardContent>
-                <Typography>Total: {dish.total}</Typography>
-              </CardContent>
-              <CardActions disableSpacing>
-                <IconButton color="secondary" aria-label="add to favorites">
-                  <DeleteIcon />
-                </IconButton>
-                <IconButton color="primary" aria-label="share">
-                  <EditIcon />
-                </IconButton>
-              </CardActions>
-            </Card>
-          </Grid>
-        ))}
+        {dishes.map((dish) => {
+          if (dish.section === filteredOption) {
+            return (
+              <Grid key={dish.name} item xs={12} md={6} lg={4}>
+                <Card elevation={5}>
+                  <CardHeader
+                    className={classes["p:firstLetter"]}
+                    title={dish.name}
+                    subheader={dish.section}
+                  ></CardHeader>
+                  <CardContent>
+                    <Typography>Total: {dish.total}</Typography>
+                  </CardContent>
+                  <CardActions disableSpacing>
+                    <IconButton color="secondary" aria-label="add to favorites">
+                      <DeleteIcon />
+                    </IconButton>
+                    <IconButton color="primary" aria-label="share">
+                      <EditIcon />
+                    </IconButton>
+                  </CardActions>
+                </Card>
+              </Grid>
+            );
+          }
+          if (filteredOption === "all") {
+            return (
+              <Grid key={dish.name} item xs={12} md={6} lg={4}>
+                <Card elevation={5}>
+                  <CardHeader
+                    className={classes["p:firstLetter"]}
+                    title={dish.name}
+                    subheader={dish.section}
+                  ></CardHeader>
+                  <CardContent>
+                    <Typography>Total: {dish.total}</Typography>
+                  </CardContent>
+                  <CardActions disableSpacing>
+                    <IconButton color="secondary" aria-label="add to favorites">
+                      <DeleteIcon />
+                    </IconButton>
+                    <IconButton color="primary" aria-label="share">
+                      <EditIcon />
+                    </IconButton>
+                  </CardActions>
+                </Card>
+              </Grid>
+            );
+          }
+        })}
       </Grid>
     </Container>
   );
