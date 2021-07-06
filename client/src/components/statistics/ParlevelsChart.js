d170e9a723cccd126e1b5aa7d2779433cd96a88d
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2021-07-06 18:14:44 +0100

    generate chart da

--- client/src/components/statistics/ParlevelsChart.js
+++ client/src/components/statistics/ParlevelsChart.js
@@ -5,8 +5,53 @@ import { Bar } from "react-chartjs-2";
 export const ParlevelsChart = (props) => {
   const { soldItems, dish } = props;
 
-  const data = {
-    labels: [
+  const translateToDays = (arrayToFilter) => {
+    const daysData = arrayToFilter.map((item) => {
+      const { sold } = item;
+
+      return {
+        day: new Date(item.date).toLocaleDateString(navigator.language, {
+          weekday: "long",
+        }),
+        sold,
+      };
+    });
+    return daysData;
+  };
+
+  const filterSoldItemsPerDay = (myDay) => {
+    const allDays = soldItems && translateToDays(soldItems);
+
+    if (allDays && allDays.length > 0) {
+      const day = allDays.filter((day) => day.day === myDay);
+      return day;
+    }
+  };
+
+  // get array of sold amounts per current dish and per day
+  const matchDayToDish = (stringDay) => {
+    const soldOnDay = filterSoldItemsPerDay(stringDay);
+
+    const dishData = soldOnDay && soldOnDay.map((day) => day.sold).flat();
+
+    const matchToDish =
+      dishData &&
+      dish &&
+      dishData
+        .map((record) => {
+          if (record.dishId !== dish._id) {
+            return null;
+          }
+          return record.sold;
+        })
+        .filter((i) => i !== null);
+
+    return matchToDish;
+  };
+
+  // Next - calculate averages ie: [1,2,3].addTogether && divide by length
+  const calculateParlevels = () => {
+    const days = [
       "Monday",
       "Tuesday",
       "Wednesday",
@@ -14,28 +59,48 @@ export const ParlevelsChart = (props) => {
       "Friday",
       "Saturday",
       "Sunday",
-    ],
+    ];
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    const parlevels = days.map((day) => {
+      const soldOnDay = matchDayToDish(day);
+      const totalAmount = soldOnDay && soldOnDay.reduce(reducer, 0);
+      const parlevel =
+        soldOnDay && soldOnDay.length > 0
+          ? Math.round(totalAmount / soldOnDay.length)
+          : 0;
+
+      return {
+        day,
+        parlevel,
+      };
+    });
+    return parlevels;
+  };
+
+  const generateChartLabels = (labelsArray) => {
+    return labelsArray.map(({ day }) => day);
+  };
+
+  const generateChartData = (labelsArray) => {
+    return labelsArray.map(({ parlevel }) => parlevel);
+  };
+  const chartLabels = generateChartLabels(calculateParlevels());
+  const chartData = generateChartData(calculateParlevels());
+
+  const data = {
+    labels: chartLabels,
     datasets: [
       {
         label: "Par level",
-        data: [12, 19, 3, 5, 2, 3, 6],
+        data: chartData,
         backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
+          "rgba(255, 99, 132, 0.7)",
+          "rgba(54, 162, 235, 0.7)",
+          "rgba(255, 206, 86, 0.7)",
+          "rgba(75, 192, 192, 0.7)",
+          "rgba(153, 102, 255, 0.7)",
+          "rgba(255, 159, 64, 0.7)",
         ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
-        borderWidth: 1,
       },
     ],
   };
@@ -52,8 +117,6 @@ export const ParlevelsChart = (props) => {
     },
   };
 
-  console.log(soldItems, dish);
-
   return (
     <div>
       <Bar data={data} options={options} />
@@ -62,7 +125,7 @@ export const ParlevelsChart = (props) => {
 };
 
 const mapStateToProps = (state) => ({
-  soldItems: Object.keys(state.soldItems),
+  soldItems: Object.values(state.soldItems),
 });
 
 export default connect(mapStateToProps, {})(ParlevelsChart);
