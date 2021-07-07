fad9ccfd129342e69f33c6c5be11ac89918ebc6f
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2021-07-07 21:38:10 +0100

    calculate parlevels for all dishes

--- client/src/components/statistics/Parlevels.js
+++ client/src/components/statistics/Parlevels.js
@@ -33,57 +33,85 @@ const Parlevels = (props) => {
   //     },
   //   ];
 
-  // 1. Transform document date to day in a week
-  const sortByDate = () => {
-    const translateToDays = soldItems.map((item) => {
-      const date = new Date(item.date);
+  const _parlevels = dishes.map((dish) => {
+    const translateToDays = (arrayToFilter) => {
+      const daysData = arrayToFilter.map((item) => {
+        const { sold } = item;
 
-      return {
-        [getDay(date)]: item.sold,
-      };
-    });
-
-    // 2. Return all documents for a day in a week
-    const matchItemsToDays = days.map((day) => {
-      const dayValues = translateToDays
-        .map((entry) => {
-          if (day === Object.keys(entry).toString()) {
-            return Object.values(entry).flat();
-          }
-          return null;
-        })
-        .flat()
-        .filter((n) => n !== null);
-
-      return {
-        [day]: dayValues,
-      };
-    });
-
-    // 3. Match documents to dish
-    const matchDishToDocument = () => {
-      const getSoldValues = matchItemsToDays.map((day) => {
-        return Object.values(day)
-          .flat()
-          .map((value) => {
-            return {
-              id: value.dishId,
-              sold: value.sold,
-            };
-          });
+        return {
+          day: new Date(item.date).toLocaleDateString(navigator.language, {
+            weekday: "long",
+          }),
+          sold,
+        };
       });
-
-      const matched = dishes.map(dish => {
-        if(dish._id === )
-
-      })
-      return getSoldValues;
+      return daysData;
     };
-    console.log(matchDishToDocument());
-    return matchItemsToDays;
-  };
 
-  sortByDate();
+    const filterSoldItemsPerDay = (myDay) => {
+      const allDays = soldItems && translateToDays(soldItems);
+
+      if (allDays && allDays.length > 0) {
+        const day = allDays.filter((day) => day.day === myDay);
+        return day;
+      }
+    };
+
+    // get array of sold amounts per current dish and per day
+    const matchDayToDish = (stringDay) => {
+      const soldOnDay = filterSoldItemsPerDay(stringDay);
+
+      const dishData = soldOnDay && soldOnDay.map((day) => day.sold).flat();
+
+      const matchToDish =
+        dishData &&
+        dish &&
+        dishData
+          .map((record) => {
+            if (record.dishId !== dish._id) {
+              return null;
+            }
+            return record.sold;
+          })
+          .filter((i) => i !== null);
+
+      return matchToDish;
+    };
+
+    const calculateParlevels = () => {
+      const days = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+      ];
+      const reducer = (accumulator, currentValue) => accumulator + currentValue;
+      const parlevels = days.map((day) => {
+        const soldOnDay = matchDayToDish(day);
+        const totalAmount = soldOnDay && soldOnDay.reduce(reducer, 0);
+        const parlevel =
+          soldOnDay && soldOnDay.length > 0
+            ? Math.round(totalAmount / soldOnDay.length)
+            : 0;
+
+        return {
+          day,
+          parlevel,
+        };
+      });
+      console.log(parlevels);
+      return parlevels;
+    };
+    return {
+      id: dish._id,
+      name: dish.name,
+      parlevels: calculateParlevels(),
+    };
+  });
+  console.log(_parlevels);
 
   return <div>ParLevels</div>;
 };
