34969d98a8752d21ddb20bf2020609d6c608975f
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2021-07-19 14:39:37 +0100

    move buffer dialog to morning count

--- client/src/components/dishes/DishCount.js
+++ client/src/components/dishes/DishCount.js
@@ -1,11 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
-import {
-  changeHeaderTitle,
-  fetchDishes,
-  updateDish,
-  getMenuOptions,
-} from "../../actions";
+import { changeHeaderTitle, fetchDishes, updateDish } from "../../actions";
 
 // Components
 import TableRow from "./TableRow";
@@ -17,44 +12,18 @@ import AppBar from "@material-ui/core/AppBar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import SwipeableViews from "react-swipeable-views";
-import {
-  Table,
-  TableBody,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogContentText,
-  TextField,
-  DialogActions,
-  Button,
-  InputAdornment,
-} from "@material-ui/core";
+import { Table, TableBody } from "@material-ui/core";
 
 const DishCount = (props) => {
-  const { getMenuOptions, changeHeaderTitle, fetchDishes, dishes, time } =
-    props;
+  const { changeHeaderTitle, fetchDishes, dishes, time, buffer } = props;
   const theme = useTheme();
 
   //State
   const [value, setValue] = useState(0);
-  const [buffer, setBuffer] = useState(10);
-  const [bufferSelected, setBufferSelected] = useState(10);
-  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     fetchDishes();
   }, [fetchDishes]);
-
-  useEffect(() => {
-    const menuOptions = [
-      {
-        text: "Add buffer",
-        action: () => setOpen(true),
-      },
-    ];
-
-    getMenuOptions(menuOptions);
-  }, [getMenuOptions]);
 
   useEffect(() => {
     changeHeaderTitle(
@@ -72,56 +41,10 @@ const DishCount = (props) => {
     fetchDishes();
   };
 
-  const handleBufferChange = (e) => {
-    setBufferSelected(e.target.value);
-  };
-
-  const handleBufferSubmit = () => {
-    setBuffer(parseInt(bufferSelected));
-    setOpen(false);
-  };
-
-  const handleClose = () => setOpen(false);
-
   const sections = ["teppan", "wok", "fry"];
 
   return (
     <div>
-      <Dialog maxWidth="xs" fullWidth open={open} onClose={handleClose}>
-        <DialogTitle id="form-dialog-title">Buffer</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Please add buffer to your recommended parlevels.
-            <br />
-            Default: 10%
-          </DialogContentText>
-          <TextField
-            autoFocus
-            color="secondary"
-            margin="dense"
-            id="name"
-            label="Buffer"
-            type="number"
-            name="buffer"
-            fullWidth
-            value={bufferSelected}
-            onChange={handleBufferChange}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">%</InputAdornment>
-              ),
-            }}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleBufferSubmit} color="primary">
-            Confirm
-          </Button>
-        </DialogActions>
-      </Dialog>
       <AppBar position="sticky" style={{ marginBottom: "2rem" }}>
         <Tabs
           onChange={handleChange}
@@ -144,19 +67,17 @@ const DishCount = (props) => {
             <TabPanel key={index} value={value} index={index}>
               <Table>
                 <TableBody>
-                  {dishes.map((dish) => {
-                    if (dish.section === section) {
-                      return (
+                  {dishes.map(
+                    (dish) =>
+                      dish.section === section && (
                         <TableRow
                           buffer={buffer}
                           time={time}
                           key={dish._id}
                           dish={dish}
                         />
-                      );
-                    }
-                    return null;
-                  })}
+                      )
+                  )}
                 </TableBody>
               </Table>
             </TabPanel>
@@ -175,5 +96,4 @@ export default connect(mapStateToProps, {
   changeHeaderTitle,
   fetchDishes,
   updateDish,
-  getMenuOptions,
 })(DishCount);
--- client/src/components/dishes/MorningCount.js
+++ client/src/components/dishes/MorningCount.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import { getMenuOptions } from "../../actions";
 
 import DishCount from "./DishCount";
 
 // Material UI Core
-import { Fab, Zoom, makeStyles } from "@material-ui/core";
+import {
+  Fab,
+  Zoom,
+  makeStyles,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  TextField,
+  DialogActions,
+  Button,
+  InputAdornment,
+} from "@material-ui/core";
 
 // Material UI Icons
 import SendIcon from "@material-ui/icons/Send";
@@ -22,10 +36,73 @@ const useStyles = makeStyles((theme) => ({
 
 const MorningCount = (props) => {
   const classes = useStyles();
+  const { getMenuOptions } = props;
+
+  const [buffer, setBuffer] = useState(10);
+  const [bufferSelected, setBufferSelected] = useState(10);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const menuOptions = [
+      {
+        text: "Add buffer",
+        action: () => setOpen(true),
+      },
+    ];
+
+    getMenuOptions(menuOptions);
+  }, [getMenuOptions]);
+
+  // Try to put parlevels logic here
+  const handleBufferChange = (e) => {
+    setBufferSelected(e.target.value);
+  };
+
+  const handleBufferSubmit = () => {
+    setBuffer(parseInt(bufferSelected));
+    setOpen(false);
+  };
+
+  const handleClose = () => setOpen(false);
 
   return (
     <div>
-      <DishCount time={"morning"} />
+      <Dialog maxWidth="xs" fullWidth open={open} onClose={handleClose}>
+        <DialogTitle id="form-dialog-title">Buffer</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Please add buffer to your recommended parlevels.
+            <br />
+            Default: 10%
+          </DialogContentText>
+          <TextField
+            autoFocus
+            color="secondary"
+            margin="dense"
+            id="name"
+            label="Buffer"
+            type="number"
+            name="buffer"
+            fullWidth
+            value={bufferSelected}
+            onChange={handleBufferChange}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">%</InputAdornment>
+              ),
+            }}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleBufferSubmit} color="primary">
+            Confirm
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <DishCount time={"morning"} buffer={buffer} />
       <Zoom timeout={650} in>
         <Fab
           variant="extended"
@@ -42,4 +119,4 @@ const MorningCount = (props) => {
   );
 };
 
-export default MorningCount;
+export default connect(null, { getMenuOptions })(MorningCount);
