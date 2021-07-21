5a463d176b89dec55455f7f16d99a94c587132a6
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2021-07-21 15:59:28 +0100

    clean up

--- client/src/components/dishes/TableRow.js
+++ client/src/components/dishes/TableRow.js
@@ -1,7 +1,6 @@
-import React, { useEffect } from "react";
+import React from "react";
 import axios from "axios";
 import { connect } from "react-redux";
-import { fetchSoldItems } from "../../actions";
 import { useAllParlevels } from "../../utils/useParlevels";
 
 // Material UI Core
